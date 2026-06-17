6268d5a06d56dfd84787c4c1c8d0e5b5c7995c82
holic614 <ijykiller@gmail.com>
2026-06-18 07:41:36 +0900

    Update service worker to version 10 and cache URLs

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,31 @@
-const CACHE_NAME = 'risk-assessment-v9';
+const CACHE_NAME = 'risk-assessment-v10';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './ICON.png'
+];
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
-
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        './manifest.json',
-        './service-worker.js'
-      ]);
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => caches.delete(key)));
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
     }).then(() => self.clients.claim())
   );
 });
-
-// 🔥 핵심: Network First 전략
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request).catch(() => {
